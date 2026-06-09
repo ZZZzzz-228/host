cb263a27e5f4ff3652e43ca927aae17cf72969e6
Nine900 <voroshilov-2006@inbox.ru>
2026-06-09 22:54:01 +0700

    com

--- _user_zip_fix/wordpress_aksibgu/public_html/admin/modals/modal-app.php
+++
@@ -1,6 +0,0 @@
-<!-- ═══ MODAL: APPLICATION VIEW ═══ -->
-<div class="mo" id="mApp"><div class="modal mlg">
-  <div class="mh"><div class="mt">Заявка</div><button class="mc" onclick="cm('mApp')"><i class="fas fa-times"></i></button></div>
-  <div class="mb" id="mAppBody"></div>
-  <div class="mf" id="mAppFoot"></div>
-</div></div>
